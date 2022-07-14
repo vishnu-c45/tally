9105e01ec4857d42e986da3d2ce45f7f3067911f
vishnu-c45 <vishnushoranur701544@gmail.com>
2022-07-14 16:48:10 +0530

    18th commit

--- tapp/models.py
+++ tapp/models.py
@@ -1,4 +1,7 @@
 
+from argparse import _MutuallyExclusiveGroup
+from bdb import effective
+from pyexpat import model
 from django.db import models
 
 
@@ -106,6 +109,60 @@ class create_goddown(models.Model):
     name=models.CharField(max_length=225)
     alias=models.CharField(max_length=225)
     under=models.CharField(max_length=225)
+
+
+class create_payhead(models.Model):
+    name=models.CharField(max_length=225)
+    alias=models.CharField(max_length=225)
+    pay_type=models.CharField(max_length=225)
+    income_type=models.CharField(max_length=225)
+    under=models.CharField(max_length=225)
+    affect_net=models.CharField(max_length=225)
+    payslip=models.CharField(max_length=225)
+    calculation_of_gratuity=models.CharField(max_length=225)
+    cal_type=models.CharField(max_length=225)
+    calculation_period=models.CharField(max_length=225)
+    leave_withpay=models.CharField(max_length=225)
+    leave_with_out_pay=models.CharField(max_length=225)
+    production_type=models.CharField(max_length=225)
+    opening_balance=models.CharField(max_length=225)
+
+
+
+
+class compute_information(models.Model):
+    Pay_head_id = models.ForeignKey(create_payhead, on_delete=models.CASCADE, null=True, blank=True)
+    compute=models.CharField(max_length=225,default="Null")
+    effective_from=models.CharField(max_length=225,default="NULL")
+    amount_greater=models.CharField(max_length=225,default="NULL")
+    amount_upto=models.CharField(max_length=225,default="NULL")
+    slab_type=models.CharField(max_length=225,default="NULL")
+    value=models.CharField(max_length=225,default="NULL")
+
+
+
+class Rounding(models.Model):
+    pay_head_id = models.ForeignKey(create_payhead, on_delete=models.CASCADE, null=True, blank=True)
+    Rounding_Method =models.CharField(max_length=225,default="Null",blank=True)
+    Round_limit = models.CharField(max_length=22,default="Null",blank=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
 
 
     
