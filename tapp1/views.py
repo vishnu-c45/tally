89a07305424ef9678e37b783ecdc8dda854f1367
vishnu-c45 <vishnushoranur701544@gmail.com>
2022-08-18 16:16:33 +0530

    first commit

---
+++ tapp1/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request,'base.html')
+
+def profit(request):
+    return render(request,'profit.html') 
+
+
+def stockcate(request):
+    return render(request,'stockcategory.html')        
+
